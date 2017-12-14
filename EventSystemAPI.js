ff8a84b84b528ef611e6854375aa6dc906fb171a
MitkoZ <mitkoz15@abv.bg>
2017-12-14 19:17:36 +0200

    Code refactored with associative arrays and trigger function implemented

--- EventSystemAPI.js
+++ EventSystemAPI.js
@@ -1,42 +1,27 @@
 
 var EventReg=[];
+var EventQueue=[];
 
 function Attach(event, callback) {
-	for (var i = 0; i < EventReg.length; i++) {
-		var currentItem = EventReg[i];
-		if (currentItem.key == event) { //EventReg already contains the event
-			currentItem.value.push(callback);
-			return;
-		}	
+	if (EventReg[event] == undefined)
+	{
+		EventReg[event] = [];
 	}
-	var regItem={key:event,value:[]}; //EventReg doesn't contain the event
-	regItem.value.push(callback);
-	EventReg.push(regItem);
+	EventReg[event].push(callback);
 }
 
 function Detach(event, callback){
-	for (var indexOfEvent = 0; indexOfEvent < EventReg.length; indexOfEvent++) {
-		var currentItem=EventReg[indexOfEvent];
-		if (currentItem.key == event && currentItem.value.includes(callback)) {
-			var indexOfCallback=currentItem.value.indexOf(callback);
-			currentItem.value.splice(indexOfCallback, 1);
-
-			if (currentItem.value.length==0) { //the event isn't associated with anymore callbacks
-				EventReg.splice(indexOfEvent, 1);
-			}
-		}
-	}
+	if(EventReg[event]!=undefined && EventReg[event].indexOf(callback)>-1){
+		EventReg[event].splice(EventReg[event].indexOf(callback),1);
+	}// removes the callback associated with the event
 }
 
 function DetachAll(event) {
-	for (var indexOfEvent = 0; indexOfEvent < EventReg.length; indexOfEvent++) {
-		currentItem=EventReg[indexOfEvent];
-		if (currentItem.key==event) {
-			EventReg.splice(indexOfEvent,1);
-		}
+	if (EventReg[event]!=undefined) {
+		EventReg[event]=[];
 	}
 }
 
 function Trigger(event, params) {
-	// body...
+	EventQueue.push({event:event,params:params});
 }
